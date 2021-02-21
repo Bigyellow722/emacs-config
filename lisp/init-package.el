0eb9fbd1c6e87b0ed3077542ff1cdf971debc476
Bigyellow722 <lhkwqy@qq.com>
2021-02-21 16:12:45 +0000

    [emacs][configuration] use ivy counsel and swiper

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -8,5 +8,49 @@
   :init (benchmark-init/activate)
   :hook (after-init . benchmark-init/deactivate))
 
+;; delete line
+(use-package crux
+  :bind ("C-c k" . crux-smart-kill-line))
+
+;; delete space region
+(use-package hungry-delete
+  :bind (("C-c DEL" . hungry-delete-backward))
+  :bind (("C-c d" . hungry-delete-forward)))
+
+;; move region or line
+(use-package drag-stuff
+  :bind (("<M-up>" . drag-stuff-up)
+	 ("<M-down>" . drag-stuff-down)))
+
+;; ivy
+(use-package ivy
+  :defer 1
+  :demand
+  :hook (after-init . ivy-mode)
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+	ivy-initial-inputs-alist nil
+	ivy-count-format "%d/%d "
+	enable-recursive-minibuffers t
+	ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+
+;; counsel
+(use-package counsel
+  :after (ivy)
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-c f" . counsel-recentf)
+	 ("C-c g" . counsel-git)))
+
+;; swiper
+(use-package swiper
+  :after (ivy)
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper-isearch-backward))
+  :config (setq swiper-action-recenter t
+		swiper-include-line-number-in-search t))
+
+
 
 (provide 'init-package)
