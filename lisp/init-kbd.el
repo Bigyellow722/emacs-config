2848d52a4439be3c5dcabe3ad228cb143fcdac7f
Bigyellow722 <lhkwqy@qq.com>
2022-04-19 14:49:08 +0800

    [configuration] set keymap for highlight-symbol and imenu-list
    Signed-off-by: Bigyellow722 <lhkwqy@qq.com>

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -5,4 +5,13 @@
 (global-set-key (kbd "C-=") 'text-scale-decrease)
 (global-set-key (kbd "C-0") 'text-scale-adjust)
 
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+
+(require 'imenu-list)
+(global-set-key [(control f1)] 'imenu-list-minor-mode)
+
 (provide 'init-kbd)
