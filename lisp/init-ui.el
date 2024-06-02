daebfccf51f4467ceb6f3926bdd3785a8388f22f
Bigyellow722 <lhkwqy@qq.com>
2024-06-02 21:09:46 +0800

    change ui
    Signed-off-by: Bigyellow722 <lhkwqy@qq.com>

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -25,6 +25,31 @@
   (dolist (elisp-code setting)
     (eval elisp-code)))
 
+
+(require 'modus-themes)
+
+
+;; Configure the Modus Themes' appearance
+(setq modus-themes-mode-line '(accented borderless)
+      modus-themes-bold-constructs t
+      modus-themes-italic-constructs t
+      modus-themes-fringes 'subtle
+      modus-themes-tabs-accented t
+      modus-themes-paren-match '(bold intense)
+      modus-themes-prompts '(bold intense)
+      modus-themes-org-blocks 'tinted-background
+      modus-themes-scale-headings t
+      modus-themes-region '(bg-only)
+      modus-themes-headings
+      '((1 . (rainbow overline background 1.4))
+        (2 . (rainbow background 1.3))
+        (3 . (rainbow bold 1.2))
+        (t . (semilight 1.1))))
+
+;; Load the dark theme by default
+(load-theme 'modus-vivendi t)
+
+
 ;;; add plugins
 ;;;; smart-mode
 (add-plugins-for-common '(use-package smart-mode-line
@@ -46,8 +71,6 @@
 			     (set-face-attribute 'default nil :height 130)
 			     )))
 
-(add-plugins-for-gui '(use-package gruvbox-theme
-			:init (load-theme 'gruvbox-dark-soft t)))
 
 
 
