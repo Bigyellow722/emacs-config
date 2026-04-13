a088021bbe6f2c7e2d981ddc22c65d67a08dad27
Bigyellow722 <lhkwqy@qq.com>
2026-04-13 14:19:56 +0800

    add sudo edit cmd
    Signed-off-by: Bigyellow722 <lhkwqy@qq.com>

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -5,5 +5,17 @@
 (global-set-key (kbd "C-=") 'text-scale-decrease)
 (global-set-key (kbd "C-0") 'text-scale-adjust)
 
+(defun er-sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 
 (provide 'init-kbd)
