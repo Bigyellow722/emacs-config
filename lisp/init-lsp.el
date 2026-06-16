c97f34b4435f03b211e40b55ccfcac69dfdc17c2
Qinyuan <lhkwqy@qq.com>
2026-06-16 14:09:46 +0800

    forbid auto-modify in c-mode
    Signed-off-by: Qinyuan <lhkwqy@qq.com>

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,10 +1,12 @@
 
-
 (use-package eglot
   :config
-  (add-to-list 'eglot-server-programs `((c-mode c++-mode c-ts-mode c++-ts-mode)  "clangd"))
+  (add-to-list 'eglot-server-programs '((c-mode c++-mode c-ts-mode c++-ts-mode) . ("clangd"
+										   "--header-insertion=never"
+										   "--header-insertion-decorators=0")))
   (add-hook 'c++-mode-hook 'eglot-ensure)
   (add-hook 'c-mode-hook 'eglot-ensure)
 )
+
 
 (provide 'init-lsp)
